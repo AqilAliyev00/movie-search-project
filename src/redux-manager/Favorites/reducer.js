82c2f09942b09442f99364c5d9f366e981ca6073
Macbook <macbook2020@172-10-190-166.lightspeed.gnvlsc.sbcglobal.net>
2022-05-18 20:06:30 +0400

    add-delite-button-fixed

--- src/redux-manager/Favorites/reducer.js
+++ src/redux-manager/Favorites/reducer.js
@@ -5,8 +5,12 @@ const initialValues = {
 };
 
 const favoritesReducer = (state = initialValues, action) => {
+  const isElementExist = state.favoriteMovies.some(
+    (el) => el?.imdbID === action.payload?.imdbID
+  );
   switch (action.type) {
     case ADD_MOVIE:
+      if (isElementExist) return state;
       return {
         ...state,
         favoriteMovies: [...state.favoriteMovies, action.payload],
