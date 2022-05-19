f2171599cdc48911c67e2185e6e69a6a2ee4c178
Macbook <macbook2020@Macbooks-MacBook-Pro.local>
2022-05-19 14:18:15 +0400

    fixed-saveList-button

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -4,20 +4,28 @@ import { useSelector } from "react-redux";
 import { addMovieList } from "../../redux-manager/Favorites/selectors";
 import { useDispatch } from "react-redux";
 import { deleteMovieListAction } from "../../redux-manager/Favorites/actions";
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
   const [title, setTitle] = useState("");
+  const [type, setType] = useState(true);
+  const [click, setClick] = useState(false);
 
   const movies = useSelector(addMovieList);
 
   const onChangeTitle = (e) => {
     setTitle(e.target.value);
+    e.target.value !== "" && movies[0] ? setType(false) : setType(true);
   };
 
   const dispatch = useDispatch();
 
   const deleteBtn = (e) => {
     dispatch(deleteMovieListAction(e.target.id));
+  };
+
+  const saveBtn = () => {
+    setClick(true);
   };
   return (
     <div className="favorites">
@@ -40,9 +48,19 @@ const Favorites = () => {
             );
           })}
       </ul>
-      <button type="button" className="favorites__save">
-        Сохранить список
-      </button>
+
+      {click ? (
+        <Link to="/list/:id">Перейти к списку</Link>
+      ) : (
+        <button
+          type="button"
+          className="favorites__save"
+          disabled={type}
+          onClick={saveBtn}
+        >
+          Сохранить список
+        </button>
+      )}
     </div>
   );
 };
